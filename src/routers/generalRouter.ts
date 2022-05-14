e6cee7a2127e506aca0a0776a3c563fc00b6de0d
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-14 18:31:20 +0700

    add route: get categories by pass tree length

--- src/routers/generalRouter.ts
+++ src/routers/generalRouter.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { addCategory, testAuthorizationToken_user, testAuthorizationToken_admin } from '../controllers/generalController'
+import { addCategory, testAuthorizationToken_user, testAuthorizationToken_admin, getCategoriesByTreeLength } from '../controllers/generalController'
 import express from 'express'
 import { authenToken, isAdmin } from '../utils/authServices';
 
@@ -9,5 +9,6 @@ const generalRouter = express.Router()
 generalRouter.post('/addCategory', addCategory)
 generalRouter.get('/testAuthorizationToken_user', authenToken, testAuthorizationToken_user)
 generalRouter.get('/testAuthorizationToken_admin', authenToken, isAdmin, testAuthorizationToken_admin)
+generalRouter.get('/getCategoriesByTreeLength', getCategoriesByTreeLength)
 
 export default generalRouter
