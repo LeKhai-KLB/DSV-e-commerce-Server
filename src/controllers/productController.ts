e75d0ea43b5c00479bb9851255458a4f4299a177
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-18 16:41:01 +0700

    modified add product controller and add add update product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,16 +4,17 @@ import Product from '../models/Product'
 import Category from '../models/Category'
 import Color from '../models/Color'
 import Brand from '../models/Brand'
-import { Request, Response } from 'express'
+import Review from '../models/Review'
+import { Request, Response, NextFunction } from 'express'
 import mongoose from 'mongoose';
 
 // ADD PRODUCT
-export const addProduct = async (req: Request, res: Response) => {
+export const passingProductData = async (req: Request, res: Response,next: NextFunction) => {
     try{
         // handle categories
         const categories = req.body.categories
-        const categoriesId:Array<mongoose.Schema.Types.ObjectId> = []
-        const categoryDocs = await Category.find().where('name').in(categories).select('name')
+        const pasingCategories:Array<any> = []
+        const categoryDocs = await Category.find().where('name').in(categories).select('name tree')
             .collation({locale: 'vi', strength: 2})
         for(let i = 0; i < categories.length; i++) {
             const checkIndex = categoryDocs.findIndex(c => c.name.toLowerCase() === categories[i].toLowerCase());
@@ -22,14 +23,14 @@ export const addProduct = async (req: Request, res: Response) => {
                 tree.unshift('Root')
                 const name = categories[i]
                 const newCategory = await Category.create({
-                    name: name.charAt(0).toUpperCase() + name.slice(1), 
-                    tree: tree.map((t:any) => t.charAt(0).toUpperCase() + t.slice(1)),
+                    name: name[0].toUpperCase() + name.slice(1), 
+                    tree: tree.map((t:any) => t[0].toUpperCase() + t.slice(1)),
                     parent: tree[i <= 1 ? i:2]
-                })
-                categoriesId.push(newCategory._id)
+                }, {new: true})
+                pasingCategories.push(newCategory)
             }
             else{
-                categoriesId.push(categoryDocs[checkIndex]._id)
+                pasingCategories.push(categoryDocs[checkIndex])
             }
         }
 
@@ -59,32 +60,148 @@ export const addProduct = async (req: Request, res: Response) => {
         if(brandSearchResult === null){
             const name = req.body?.brand ? req.body.brand:'no brand'
             const newBrand = await Brand.create({
-                name: name.charAt(0).toUpperCase() + name.slice(1)
+                name: name[0].toUpperCase() + name.slice(1)
             })
             brandId = newBrand._id
         }
         else{
             brandId = brandSearchResult._id
         }
+
         
+        const categoriesId = pasingCategories.filter(c => {
+            if(c.tree.length === 3) return c._id
+        })
         // add new product
+        req.body = {
+            ...req.body,
+            categories: categoriesId,
+            brand: brandId,
+            colors: colorsId
+        }
+        next()
+    }
+    catch(err: any){
+        console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+export const addProduct = async (req: Request, res: Response) => {
+    try {
         const product = await Product.findOne({name: req.body.name})
-            .collation({locale: 'vi', strength: 2})
+                .collation({locale: 'vi', strength: 2})
         if(product === null){
-            const newProduct = await Product.create({
-                ...req.body,
-                categories: categoriesId.slice(2),
-                brand: brandId,
-                colors: colorsId
-            })
+            const newProduct = await Product.create(req.body)
             return res.json({status: 200, resultData: newProduct})
         }
         else{
             throw new Error('duplicate product name')
         }
     }
-    catch(err: any){
+    catch(err: any) {
         console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const {_id, ...productData} = req.body
+        const product = await Product.findOneAndUpdate({_id: req.body._id}, productData, {new: true})
+        if(product === null) {
+            throw new Error('Can not find current product')
+        }
+        else {
+            return res.json({status: 200, resultData: product})
+        }
+    }
+    catch(err: any) {
+        console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+// GET PRODUCTS AND SORT BY A-Z
+export const getProducts_SortAtoZ = async (req: Request, res: Response) => {
+    try {
+        const { search, first, last } = req.query
+        const products = await Product.find({name: {$regex: new RegExp(`${search}`, 'gi')}})
+        .populate(['brand', 'colors', 'categories']).collation({locale: 'vi', caseLevel: true})
+        if(products.length === 0) {
+            return res.json({ status: 404, errorMessage: 'Can not find any products'})
+        }
+        else {
+            const temp = products.map(m => m.name)
+            return res.json({status: 200, resultData: {
+                products: products.slice(Number(first), Number(last) > products.length ? products.length: Number(last)),
+                remainingLength: products.length
+            }})
+        }
+    }
+    catch(err: any) {
+        // console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+// GET PRODUCTS AND SORT BY Z-A
+export const getProducts_SortZtoA = async (req: Request, res: Response) => {
+    try {
+        const { search, first, last } = req.query
+        const products = await Product.find({name: {$regex: new RegExp(`${search}`, 'gi')}})
+        .populate(['brand', 'colors', 'categories']).collation({locale: 'vi', caseLevel: true})
+        if(products.length === 0) {
+            return res.json({ status: 404, errorMessage: 'Can not find any products'})
+        }
+        else {
+            const newProducts = products.reverse().slice(Number(first), Number(last) > products.length ? products.length: Number(last))
+            return res.json({status: 200, resultData: {
+                products: newProducts,
+                remainingLength: products.length
+            }})
+        }
+    }
+    catch(err: any) {
+        // console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+// GET ALL PRODUCTS AND SORT BY DATE ADDED
+export const getProducts_SortByDateAdded = async (req: Request, res: Response) => {
+    try {
+        const { search, first, last } = req.query
+        const products = await Product.find({name: {$regex: new RegExp(`${search}`, 'gi')}})
+            .sort({_id: -1})
+            .populate(['brand', 'colors', 'categories'])
+
+        if(products.length === 0) {
+            return res.json({ status: 404, errorMessage: 'Can not find any products'})
+        }
+        else {
+            const temp = products.map(m => m.name)
+            return res.json({status: 200, resultData: {
+                products: products.slice(Number(first), Number(last) > products.length ? products.length: Number(last)),
+                remainingLength: products.length
+            }})
+        }
+    }
+    catch(err: any) {
+        // console.log(err)
+        return res.json({status: 404, errorMessage: err.message})
+    }
+}
+
+// DELETE PRODUCT 
+export const deleteProduct = async (req: Request, res: Response) => {
+    try{
+        const {id} = req.body
+        await Review.deleteOne({productId: id})
+        await Product.deleteOne({_id: id})
+        return res.json({status: 200})
+    }
+    catch (err: any) {
         return res.json({status: 404, errorMessage: err.message})
     }
 }
