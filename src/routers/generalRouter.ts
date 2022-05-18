ba678c16d4a6d12108248f91dfb7026b84fd4383
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-18 16:41:26 +0700

    set all new routes

--- src/routers/generalRouter.ts
+++ src/routers/generalRouter.ts
@@ -1,6 +1,13 @@
 'use strict';
 
-import { addCategory, testAuthorizationToken_user, testAuthorizationToken_admin, getCategoriesByTreeLength } from '../controllers/generalController'
+import { 
+    addCategory, 
+    testAuthorizationToken_user, 
+    testAuthorizationToken_admin, 
+    getCategoriesByTreeLength, 
+    getAllBrands,
+    getAllColors
+} from '../controllers/generalController'
 import express from 'express'
 import { authenToken, isAdmin } from '../utils/authServices';
 
@@ -10,5 +17,7 @@ generalRouter.post('/addCategory', addCategory)
 generalRouter.get('/testAuthorizationToken_user', authenToken, testAuthorizationToken_user)
 generalRouter.get('/testAuthorizationToken_admin', authenToken, isAdmin, testAuthorizationToken_admin)
 generalRouter.get('/getCategoriesByTreeLength', getCategoriesByTreeLength)
+generalRouter.get('/getAllBrands', getAllBrands)
+generalRouter.get('/getAllColors', getAllColors)
 
 export default generalRouter
--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,10 +1,23 @@
 'use strict';
 
-import { addProduct } from "../controllers/productController";
+import { 
+passingProductData,
+addProduct, 
+updateProduct,
+getProducts_SortAtoZ, 
+getProducts_SortZtoA, 
+getProducts_SortByDateAdded,
+deleteProduct
+} from "../controllers/productController";
 import express from "express";
 
 const productRouter = express.Router(); 
 
-productRouter.post('/add', addProduct);
+productRouter.post('/admin/add', passingProductData, addProduct);
+productRouter.post('/admin/update', passingProductData, updateProduct);
+productRouter.get('/admin/getProducts_SortAtoZ', getProducts_SortAtoZ);
+productRouter.get('/admin/getProducts_SortZtoA', getProducts_SortZtoA);
+productRouter.get('/admin/getProducts_SortByDateAdded', getProducts_SortByDateAdded);
+productRouter.post('/admin/delete', deleteProduct);
 
 export default productRouter
