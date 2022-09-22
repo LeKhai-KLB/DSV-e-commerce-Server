fc26744a5d3faa53c3047a3aa1283e0f19cba73b
LeKhai-KLB <khaileklb1111@gmail.com>
2022-09-22 17:25:02 +0700

    add config for redis

--- src/connections/redis.ts
+++ src/connections/redis.ts
@@ -1,16 +1,22 @@
-import * as redis from "redis"
+import * as redis from "redis";
 
-const redisClient = redis.createClient()
+const redisClient = redis.createClient({
+  socket: {
+    host: process.env.REDIS_HOST as string,
+    port: Number(process.env.REDIS_PORT) || 6379,
+  },
+});
 
 function connect_redisServer() {
-    redisClient.connect()
-    .then(async() => {
-        console.log('connected to redis server')
+  redisClient
+    .connect()
+    .then(async () => {
+      console.log("connected to redis server");
     })
     .catch((err: any) => {
-        console.log(err)
-    })
+      console.log(err);
+    });
 }
 
-export default redisClient
-export { connect_redisServer }
+export default redisClient;
+export { connect_redisServer };
