983202c78724151449da49821228330a75c7f38a
LeKhai-KLB <khaileklb1111@gmail.com>
2022-09-22 17:50:44 +0700

    fix redis url

--- src/connections/redis.ts
+++ src/connections/redis.ts
@@ -1,10 +1,7 @@
 import * as redis from "redis";
 
 const redisClient = redis.createClient({
-  socket: {
-    host: process.env.REDIS_HOST as string,
-    port: Number(process.env.REDIS_PORT) || 6379,
-  },
+  url: process.env.REDIS_URL as string,
 });
 
 function connect_redisServer() {
